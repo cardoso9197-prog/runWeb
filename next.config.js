226c0d9f843deba31f17afcf416ed7366741faa1
Edivaldo Cardoso <cardoso9197@gmail.com>
2026-01-05 17:55:19 +0000

    Updated Investor and Project Reports - January 2026 Production Status

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
     domains: ['localhost', 'runrun-backend.up.railway.app', 'run-production-3070.up.railway.app'],
